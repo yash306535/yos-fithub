a51c00d0a993d986124042765bf5fb73e020c6bb
Yash30653 <yashvantmane98@gmail.com>
2024-03-02 22:34:30 +0530

    update

--- src/db/conn.js
+++ src/db/conn.js
@@ -5,6 +5,8 @@ const uri = "mongodb+srv://yashvant:yash3005@yos-fithub.ra5uf3v.mongodb.net/?ret
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
+    tls: true,
+    tlsCAFile: '/path/to/ca.pem',
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
